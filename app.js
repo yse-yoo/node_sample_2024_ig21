abd626a4a299ca622b657574d9ccf9b9178acf75
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-03 14:17:53 +0900

    update

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 // ポート番号
 const port = 3000;
 
+// Routing（ルーティング）
+// WebRoot「/」にGETリクエストしたら動作
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js page.');
+})
+
 // サーバ待機（server listen)
 app.listen(port, () => {
     console.log(`Server listen http://localhost:${port}`)
